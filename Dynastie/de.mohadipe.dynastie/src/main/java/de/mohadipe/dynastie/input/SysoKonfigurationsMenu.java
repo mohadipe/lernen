73839b44acc69e25192d1a686675685d962c8311
mohadipe <github@mohadipe.de>
2015-07-24 16:10:42 +0200

    Auswahl der beteiligten Spieler via Menu.

--- Dynastie/de.mohadipe.dynastie/src/main/java/de/mohadipe/dynastie/input/SysoKonfigurationsMenu.java
+++ Dynastie/de.mohadipe.dynastie/src/main/java/de/mohadipe/dynastie/input/SysoKonfigurationsMenu.java
@@ -22,21 +22,60 @@ public class SysoKonfigurationsMenu extends SysoTutNixInput {
 		System.out.println("/ " + spielKonfiguration.getEinheitenPunkte());
 		System.out.println("Welche Siegbedingungen?");
 		System.out.println("/ " + spielKonfiguration.getSieg());
-		System.out.println("EinzelSpieler-Modus gegen Computer");
-		System.out.println("Ihr Name:");
+		System.out.println("1: Mensch gegen Computer");
+		System.out.println("2: Mensch gegen Mensch");
+		System.out.println("3: Computer gegen Computer");
+
 		try {
-			String nameMensch = br.readLine();
-			Spieler menschSpieler = new MenschSpieler();
-			menschSpieler.setIdentitaet(nameMensch);
-			Spieler computerSpieler = new ComputerSpieler();
-			spielKonfiguration.getAlleSpieler().add(menschSpieler);
-			spielKonfiguration.getAlleSpieler().add(computerSpieler);
+			int auswahl = Integer.parseInt(br.readLine());
+			switch (auswahl) {
+			case 1:
+				menschVsComputer(br);
+				break;
+			case 2:
+				menschVsMensch(br);
+				break;
+			case 3:
+				computerVsComputer(br);
+				break;
+			default:
+				menschVsComputer(br);
+				break;
+			}
 			System.out.println("15 Weiter");
 			System.out.println("2 Beenden");
 			naechsterSchritt = Integer.parseInt(br.readLine());
 		} catch (NumberFormatException | IOException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private void computerVsComputer(BufferedReader br) {
+		spielKonfiguration.getAlleSpieler().add(new ComputerSpieler("KI1"));
+		spielKonfiguration.getAlleSpieler().add(new ComputerSpieler("KI2"));
+	}
+
+	private void menschVsMensch(BufferedReader br) throws IOException {
+		System.out.println("Ihr Name 1. Spieler:");
+		String nameMensch1 = br.readLine();
+		Spieler menschSpieler1 = new MenschSpieler();
+		menschSpieler1.setIdentitaet(nameMensch1);
+		System.out.println("Ihr Name 2. Spieler:");
+		String nameMensch2 = br.readLine();
+		Spieler menschSpieler2 = new MenschSpieler();
+		menschSpieler2.setIdentitaet(nameMensch2);
+		spielKonfiguration.getAlleSpieler().add(menschSpieler1);
+		spielKonfiguration.getAlleSpieler().add(menschSpieler2);
+	}
+
+	private void menschVsComputer(BufferedReader br) throws IOException {
+		System.out.println("Ihr Name:");
+		String nameMensch = br.readLine();
+		Spieler menschSpieler = new MenschSpieler();
+		menschSpieler.setIdentitaet(nameMensch);
+		Spieler computerSpieler = new ComputerSpieler();
+		spielKonfiguration.getAlleSpieler().add(menschSpieler);
+		spielKonfiguration.getAlleSpieler().add(computerSpieler);
 	}
 
 	@Override
