3067cae027110a07a99d34a98c08f3c0a51ffe09
mohadipe <github@mohadipe.de>
2015-10-25 14:41:17 +0100

    DynastieUI
    Initial erstellt. UI mit libgdx für mein Spiel Dynastie.

---
+++ DynastieUI/desktop/src/de/mohadipe/dynastie/ui/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.mohadipe.dynastie.ui.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.mohadipe.dynastie.ui.DynastieUI;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DynastieUI(), config);
+	}
+}
